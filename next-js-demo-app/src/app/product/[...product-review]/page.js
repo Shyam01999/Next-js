2901a585bc9849ae8bbc87c1a60b485e472209bb
shyam1998 <shyamsundarsahoo1998@gmail.com>
2025-02-21 01:35:10 +0530

    dynamic routes and catch all routes

---
+++ next-js-demo-app/src/app/product/[...product-review]/page.js
@@ -0,0 +1,7 @@
+function ProductReview() {
+    return (
+        <h1>This is a Product Review page.This page is used to catch all routes.</h1>
+    )
+}
+
+export default ProductReview; 
---
+++ next-js-demo-app/src/app/product/[details]/page.js
@@ -0,0 +1,9 @@
+
+
+function ProductDetails() {
+  return (
+    <h1>This is a product details page.</h1>
+  )
+}
+
+export default ProductDetails
