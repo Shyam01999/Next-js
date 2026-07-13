c7f3ec7a785ec20cf0fefb6164062b8ed7d2e3e5
shyam01999 <shyamsundarsahoo1998@gmail.com>
2026-07-14 00:09:27 +0530

    update

--- next-js-demo-app/src/app/not-found.js
+++ next-js-demo-app/src/app/not-found.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function NotFound() {
     return (
         <>
             <h1>This page not found </h1>
+            <Link href={"home"}>Go to home page</Link>
         </>
     )
 }
